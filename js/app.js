bbbed3c32555a5539cecc345be93cd81aff5158c
Katie Haughton <34194015+khenderson14@users.noreply.github.com>
2024-09-20 08:55:23 -0700

    changes as of 9/20

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// TRAFFIC CHARTS //
+const trafficCanvas = document.getElementById('traffic-chart');
+let trafficData = {
+     labels: ['16-22', '23-29','30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
+     datasets: [{
+            data: [700, 1200, 1000, 2000, 1500, 1700, 1200, 1800, 2200, 1500, 2500],
+            backgroundColor: ['rgb(221, 222, 237)'],
+            borderWidth: 1,
+    }]
+};
+
+let trafficOptions = {
+    fill: true,
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    }
+};
+
+let trafficChart = new Chart (trafficCanvas, {
+    type: 'line',
+    data: trafficData,
+    options: trafficOptions
+
+});
+
+
+
+
+
+
+ 
