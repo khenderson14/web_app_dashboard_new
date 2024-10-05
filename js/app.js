abed4da35c7d15529f9f4ff7513dd78609f5da1f
Katie Haughton <khenderson148@gmail.com>
2024-10-05 16:22:26 -0700

    removed repeating ids

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ closeAlertButton.addEventListener('click', closeAlert);
 
 // TRAFFIC CHART //
 
-const trafficCanvas = document.getElementById('traffic-container');
+const trafficCanvas = document.getElementById('traffic-chart');
 let trafficData = {
      labels: ['16-22', '23-29','30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
      datasets: [{
