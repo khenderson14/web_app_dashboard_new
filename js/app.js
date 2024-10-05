7fc10228a7a5f860918d8a67e647e9bd36629ab5
Katie Haughton <khenderson148@gmail.com>
2024-10-05 16:07:33 -0700

    added JS for notification close

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
-// TRAFFIC CHARTS //
-const trafficCanvas = document.getElementById('traffic-chart');
+// ALERT NOTIFICATION //
+
+const alertElement = document.getElementById('alert');
+const closeAlertButton = document.querySelector('.close-alert');
+
+function closeAlert() {
+    alertElement.style.display = 'none'; 
+}
+
+closeAlertButton.addEventListener('click', closeAlert);
+
+// TRAFFIC CHART //
+
+const trafficCanvas = document.getElementById('traffic-container');
 let trafficData = {
      labels: ['16-22', '23-29','30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
      datasets: [{
